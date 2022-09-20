09210d318543a7be7550123b3d9c916602f6d8fc
sfjbssept <sfjbssept@gmail.com>
2022-09-20 15:31:58 +0000

    send json resp

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -2,7 +2,12 @@
 const http = require('http');
 
 const server = (req, res) => {
-    res.write('<h1>Hello Worlddd ...</h1>');
+    res.writeHead(200, {'content-type': 'application/json'})
+    res.write(JSON.stringify({
+        name: 'John',
+        age: 22,
+        city: 'london'
+    }));
     res.end();
 }
 http.createServer(server).listen(5000);
