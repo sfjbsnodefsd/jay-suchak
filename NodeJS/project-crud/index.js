686b2601544e03f0e903675019d928f9a4cb2266
sfjbssept <sfjbssept@gmail.com>
2022-09-21 15:15:09 +0000

    add crud with mysql

--- NodeJS/project-crud/index.js
+++ NodeJS/project-crud/index.js
@@ -25,9 +25,28 @@ let server = app.listen(5000, () => {
 app.get('/', (req, res) => {
     return res.send('Welcome Jay');
 })
-
+//  gett all
 app.get('/getEmp', async (req, res) => {
-    connect.query('select * from employeedata', (err, data, fields)=>{
+    connect.query('select * from employeedata', (err, data, fields) => {
+        if (err) throw err;
+        console.log(fields);
+        return res.send(data);
+    });
+})
+
+// get by id
+app.get('/getEmp/:id', async (req, res) => {
+    connect.query(`select * from employeedata where empId=${req.params.id}`, (err, data, fields) => {
+        if (err) throw err;
+        console.log(fields);
+        return res.send(data);
+    });
+})
+
+// delete by id
+app.delete('/getEmp/:id', async (req, res) => {
+    connect.query(`delete from employeedata where empId=${req.params.id}`, (err, data, fields) => {
+        if (err) throw err;
         console.log(fields);
         return res.send(data);
     });
