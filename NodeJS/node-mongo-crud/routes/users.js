5ae89439e51fce188425c3429839a0a4c7c0f9d8
sfjbssept <sfjbssept@gmail.com>
2022-09-26 15:21:40 +0000

    add crud with mongo

--- NodeJS/node-mongo-crud/routes/users.js
+++ NodeJS/node-mongo-crud/routes/users.js
@@ -25,4 +25,37 @@ router.post('/add', async (req, res) => {
         res.send(error)
     }
 })
+
+//  delete user by id
+router.delete('/:id', async (req, res) => {
+    try {
+        let _id = req.params.id;
+        let deleteRes = await userModel.deleteOne({
+            _id
+        });
+        return res.send({
+            message: 'user is deleted',
+            deleteRes
+        })
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+//  update user by id
+router.put('/:id', async (req, res) => {
+    try {
+        let _id = req.params.id;
+        let updateData = req.body;
+        let updateRes = await userModel.updateOne({
+            _id
+        }, updateData);
+        return res.send({
+            message: 'user is updated',
+            updateRes
+        })
+    } catch (error) {
+        res.send(error)
+    }
+})
 module.exports = router;
