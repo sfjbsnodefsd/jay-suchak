1385e2e89e08ab30e0d621770e5725d557157be2
sfjbssept <sfjbssept@gmail.com>
2022-10-12 15:42:34 +0000

    add pensionser detail from csv

--- NodeJS/pension-system-microservice/pensioner-detail-service/readPensionerData.js
+++ NodeJS/pension-system-microservice/pensioner-detail-service/readPensionerData.js
@@ -1,19 +1,43 @@
+const mongoose = require("mongoose");
+mongoose
+    .connect("mongodb://localhost:27017/PMS-pensioner-detail-service", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(console.log("[PENSIONER_DETAIL-SERVICE] - Connected to database"))
+    .catch((err) => {
+        console.error(err);
+        console.error("[PENSIONER_DETAIL-SERVICE] - DB connection failed!");
+    });
 let PensionerDetail = require('./pensionerDetail');
-let fs = require("fs"),
-    readline = require("readline");
+const csv2json = require('csvjson-csv2json');
+let fs = require("fs");
 
 let file = "pensionerDataList.csv";
 
-let rl = readline.createInterface({
-    input: fs.createReadStream(file),
-    output: null,
-    terminal: false
-})
-
-rl.on("line", function(line) {
-    console.log("Got line: " + line);
+let json = fs.readFileSync(file, "utf8");
+json = csv2json(json, {
+    parseNumbers: true
 });
+console.log(json);
 
-rl.on("close", function() {
-    console.log("All data processed.");
-});
+(async () => {
+    for (let data of json) {
+        try {
+            await PensionerDetail.create({
+                name: data.Name,
+                dob: data.DOB,
+                pan: data.PAN,
+                salary_earned: data.SalaryEarned,
+                allowances: data.Allowances,
+                classification: data.classifications,
+                "bank_detail.bank_name": data.BankName,
+                "bank_detail.account_number": data.AccountNumber,
+                "bank_detail.bank_type": data.BankType,
+                aadhaar: data.Adhaar
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    }
+})();
