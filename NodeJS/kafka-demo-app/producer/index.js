8fc297578988aff52b1a9625914361fdbab754a7
sfjbssept <sfjbssept@gmail.com>
2022-10-19 15:31:01 +0000

    kafka code

--- NodeJS/kafka-demo-app/producer/index.js
+++ NodeJS/kafka-demo-app/producer/index.js
@@ -1,1 +1,17 @@
-console.log('Producer');
+console.log("producer");
+import Kafka from "node-rdkafka";
+
+const stream = Kafka.createWriteStream({
+    "metadata.broker.list": "localhost:9092",
+}, {}, {
+    topic: "test"
+});
+
+
+function queueMessage() {
+    const result = stream.write(Buffer.from("Hey my name is nishant"))
+    console.log(result);
+}
+setInterval(() => {
+    queueMessage();
+}, 3000)
