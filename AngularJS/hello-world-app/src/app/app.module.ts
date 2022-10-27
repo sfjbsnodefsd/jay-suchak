be8e317cd28f4867385e3c725d8b5958790b80ae
sfjbssept <sfjbssept@gmail.com>
2022-10-27 13:21:59 +0000

    intregrated bootstrap and added nav bar

--- AngularJS/hello-world-app/src/app/app.module.ts
+++ AngularJS/hello-world-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserFormsComponent } from './Components/user-forms/user-forms.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormsComponent
+    UserFormsComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
