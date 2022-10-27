7991c7a75f2da5e03dc71c2948287d019ed8f097
sfjbssept <sfjbssept@gmail.com>
2022-10-27 15:23:57 +0000

    added service and post method

--- AngularJS/hello-world-app/src/app/app.module.ts
+++ AngularJS/hello-world-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +19,8 @@ import { HomeComponent } from './Components/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
