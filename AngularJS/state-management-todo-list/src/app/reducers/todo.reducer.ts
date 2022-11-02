136b7a8f1c24349fcbb0e389ee4fd69586f8ee60
sfjbssept <sfjbssept@gmail.com>
2022-11-02 13:32:51 +0000

    add todo style

--- AngularJS/state-management-todo-list/src/app/reducers/todo.reducer.ts
+++ AngularJS/state-management-todo-list/src/app/reducers/todo.reducer.ts
@@ -1,6 +1,7 @@
 import { InitialState } from "@ngrx/store/src/models";
 import { ActionParent } from "../actions/todo.actions";
 import { Todo } from "../Entity/Todo";
+import { TodoActionType } from "../shared/enum/todo-actions-enum";
 
 const initState: Todo[] = [{
         title: "Dummy Title 1"
@@ -8,6 +9,8 @@ const initState: Todo[] = [{
 
 export function TodoReducer(state = initState, action: ActionParent) {
     switch (action.type) {
+        case TodoActionType.Add:
+            return [...state, action.payload];
         default:
             return state;
             break;
