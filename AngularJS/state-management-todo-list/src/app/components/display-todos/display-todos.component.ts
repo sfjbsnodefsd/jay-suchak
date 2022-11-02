2f6c63a0094bf540febe4b139f439bba78b0a2e2
sfjbssept <sfjbssept@gmail.com>
2022-11-02 14:18:40 +0000

    remove todo add done

--- AngularJS/state-management-todo-list/src/app/components/display-todos/display-todos.component.ts
+++ AngularJS/state-management-todo-list/src/app/components/display-todos/display-todos.component.ts
@@ -1,15 +1,7 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  select,
-  Store
-} from '@ngrx/store';
-import {
-  Todo
-} from 'src/app/Entity/Todo';
-
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { TodoRemove } from 'src/app/actions/todo.actions';
+import { Todo } from 'src/app/Entity/Todo';
 @Component({
   selector: 'app-display-todos',
   templateUrl: './display-todos.component.html',
@@ -17,13 +9,13 @@ import {
 })
 export class DisplayTodosComponent implements OnInit {
   todos: Todo[];
-  constructor(private store: Store < {
-    todos: Todo[]
-  } > ) {
+  removeTodo(index: number){
+    this.store.dispatch(new TodoRemove(index));
+  }
+  constructor(private store: Store < { todos: Todo[] } > ) {
     store.pipe(select('todos')).subscribe((values) => {
       this.todos = values;
       console.log(values);
-
     })
   }
 
