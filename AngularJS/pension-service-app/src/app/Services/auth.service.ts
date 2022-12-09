bc15d33f430a8c46b3a2ee867bc28a501bdaf8c8
sfjbssept <sfjbssept@gmail.com>
2022-12-09 15:22:26 +0000

    auth login

--- AngularJS/pension-service-app/src/app/Services/auth.service.ts
+++ AngularJS/pension-service-app/src/app/Services/auth.service.ts
@@ -1,18 +1,61 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { Authdata } from '../Models/auth/auth.model';
 // import decode from 'jwt-decode';
-
+const LOGIN_URL = 'http://localhost:5001/auth/login';
 @Injectable()
 export class AuthService {
-
-  public getToken(): any {
-    return localStorage.getItem('token');
+  response: any;
+  private token: any;
+  private isAuthenticated = false;
+  private authStatusListener = new Subject<boolean>()
+  constructor(private http: HttpClient, private router: Router) { }
+  getToken() {
+    console.log(this.token);
+    return this.token;
+  }
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
+  login(username: string, password: string) {
+    const authData: Authdata = { username, password };
+    this.http.post(LOGIN_URL, authData).subscribe((response: any) => {
+      console.log(response);
+      this.token = response.data;
+      this.isAuthenticated = true;
+      this.authStatusListener.next(true);
+      this.saveAuthData(this.token);
+      this.router.navigate(['list_pensioner'])
+    })
+  }
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+  logout() {
+    this.token = '';
+    this.authStatusListener.next(false);
+    this.clearAuthData();
+    this.isAuthenticated = false;
+    this.router.navigate(['login'])
   }
 
-  public isAuthenticated(): boolean {
-    // get the token
-    const token = this.getToken();
-    // return a boolean indicating whether or not the token is expired
-    return token ? true : false;
+  private saveAuthData(token: string) {
+    localStorage.setItem("token", token)
+  }
+  private clearAuthData() {
+    console.log("Clear local storage");
+    localStorage.removeItem("token")
+  }
+  private getAuthData() {
+    return localStorage.getItem("token");
+  }
+  autoAuthUser() {
+    const token = this.getAuthData();
+    this.token = token;
+    this.isAuthenticated = true;
+    this.authStatusListener.next(true)
   }
 
 }
--- AngularJS/pension-service-app/src/app/app.component.ts
+++ AngularJS/pension-service-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './Services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) { }
   title = 'pension-service-app';
+  ngOnInit() {
+    console.log("app componenne")
+    this.authService.autoAuthUser();
+  }
 }
